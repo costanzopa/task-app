e86e21884430d51dde8ca55609430936f11b5b30
Pablo Costanzo <costanzopa@gmail.com>
2019-03-29 01:00:42 -0300

    Adding get by id Routes.

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,7 +7,19 @@ router.get('/', function(req, res, next) {
     Task.find({}).then((tasks) => {
         res.send(tasks);
     }).catch((e) => {
-        res.send(e);
+        res.status(500).send(e);
+    });
+});
+
+router.get('/:id', function(req, res, next) {
+    const _id = req.params.id;
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch((e) => {
+        res.status(500).send(e);
     });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,21 @@ router.get('/', function(req, res, next) {
   User.find({}).then((users) => {
     res.send(users);
   }).catch((e) => {
-    res.send(e);
+    res.status(500).send(e);
   });
 });
 
+router.get('/:id', function(req, res, next) {
+    const _id = req.params.id;
+    User.findById(_id).then((user) => {
+      if(!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    }).catch((e) => {
+        res.status(500).send(e);
+    });
+});
 
 router.post('/', (req, res, next) => {
   const user = new User(req.body);
