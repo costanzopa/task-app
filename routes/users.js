90cad825f8444869a32e77c25c42c04070bb91eb
Pablo Costanzo <costanzopa@gmail.com>
2019-04-03 00:35:04 -0300

    Validating avatars.

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,16 @@ const express = require('express'),
 const router  = express.Router();
 
 const avatar  = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            return cb(new Error('Please upload an image.'))
+        }
+        cb(undefined, true);
+    }
 });
 
 /* GET current profile*/
